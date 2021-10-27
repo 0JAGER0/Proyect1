c10711603165b40762684e82517358d9c2f98ea5
yurivKmirano <di.uribe@duocuc.cl>
2021-10-27 20:16:52 -0300

    cosas

--- src/app/my-servicio.service.ts
+++ src/app/my-servicio.service.ts
@@ -1,9 +1,48 @@
+import { getLocaleFirstDayOfWeek } from '@angular/common';
 import { Injectable } from '@angular/core';
+
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyServicioService {
 
-  constructor() { }
+  constructor(private storage :Storage) { 
+    this.init();
+  }
+
+  async init()
+  {
+    await this.storage.create();
+  }
+
+  async agregarConKey(key: string, valor: string)
+  {
+    await this.storage.set(key,valor);
+  }
+
+  async agregar(valor:string)
+  {
+    let id = await this.storage.length() + 1;
+    await this.storage.set(id.toString(),valor);
+  }
+
+    async reascatar(key:string)
+    {
+      return await this.storage.get(key);
+    }
+    listar()
+    {
+      let listado = []
+      this.storage.forEach((v,k) => {listado.push(v); })
+      return listado;
+    }
+
+    eliminar(key:string)
+    {
+      this.storage.remove(key);
+    }
+
+
 }
--- src/app/series/series.page.ts
+++ src/app/series/series.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SeriesService } from './series.service';
-import { StorageService, Item } from '../service/storage.service';
-import { Platform, ToastController,IonList } from '@ionic/angular';
+
 
 
 @Component({
@@ -10,73 +9,13 @@ import { Platform, ToastController,IonList } from '@ionic/angular';
   styleUrls: ['./series.page.scss'],
 })
 export class SeriesPage implements OnInit {
+  private ser =  []
+  constructor(private servicios: SeriesService) { }
 
-  items: Item [] =[];
-
-  newItem: Item = <Item>{};
-
-  @ViewChild('mylist')mylist: IonList;
-
-  private ser = []
-
-  constructor(private servicios : SeriesService, private storageService: StorageService, private plt: Platform, private toastController: ToastController) { 
-    this.plt.ready().then(() => {
-      this.loadItems();
-    });
-  }
-
-  //CREAR
-  addItem(){
-    this.newItem.modified = Date.now();
-    this.newItem.id = Date.now();
-
-    this.storageService.addItem(this.newItem).then(item => {
-      this.newItem = <Item>{};
-      this.showToast('item agregado!')
-      this.loadItems();
-
-    });
-  }
-
-  //LEER
-  loadItems(){
-    this.storageService.getItems().then(items => {
-      this.items = items;
-    });
-  }
-
-  //actualizar
-  updateItem(item: Item){
-    item.title = `actualizado: ${item.title}`;
-    item.modified = Date.now();
-
-    this.storageService.updateItem(item).then(item =>{
-      this.showToast('item actualizado!');
-      this.mylist.closeSlidingItems();
-      this.loadItems();
-    });
-  }
-
-  //borrar
-  deleteItem(item: Item){
-    this.storageService.deleteItem(item.id).then(item => {
-      this.showToast('item borrado');
-      this.mylist.closeSlidingItems();
-      this.loadItems();
-    });
-  }
-
-  // helper
-  async showToast(msg){
-    const toast = await this.toastController.create({
-      message: msg,
-      duration: 2000
-    });
-    toast.present();
-  }
 
   ngOnInit() {
     this.ser = this.servicios.getSeries();
+   
   }
 
 }
